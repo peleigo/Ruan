a7b4d1c89025c95717b2bea50b9a72b22385dd0d
peleigo <pasoca166@gmail.com>
2026-03-31 21:04:35 -0300

    pontuação funcional

--- Assets/script/ScoreManegement.cs
+++ Assets/script/ScoreManegement.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManegement : MonoBehaviour
 {
+    public GameManager gameManager;
     public static ScoreManegement Instance;
-    public Text scoreText;
+    public  TextMeshProUGUI scoreText;
     public Text HighscoreText;
 
     int score = 0;
@@ -20,6 +22,15 @@ public class ScoreManegement : MonoBehaviour
     {
         scoreText.text = score.ToString() + "POINTS";
         HighscoreText.text = score.ToString() + "Highscore: " + highscore.ToString();
+    }
+
+    private void Update()
+    {
+        if (scoreText)
+        {
+            scoreText.text = gameManager.score.ToString() + " points";
+        }
+        //scoreText.text = score.ToString() + " points";
     }
 
     public void AddPoint()
